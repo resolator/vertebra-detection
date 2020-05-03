03178602fec32f0e8ed499bb1cc0a912291c995e
Vladislav <v.dusiak@gmail.com>
2020-05-03 14:43:20 +0300

    Added support for TorchScript models in demo app

--- demo/demo_app.py
+++ demo/demo_app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
 import cv2
 import sys
 import json
@@ -9,11 +8,12 @@ import argparse
 import numpy as np
 
 from tqdm import tqdm
+from pathlib import Path
 
 import torch
 from torchvision import models
 
-sys.path.append(os.path.join(sys.path[0], '../'))
+sys.path.append(str(Path(sys.path[0]).parent))
 from common.transforms import get_test_transform, ToTensor
 from common.utils import postprocessing, draw_bboxes
 from common.evaluator import Evaluator
@@ -24,80 +24,91 @@ def get_args():
     parser = argparse.ArgumentParser(
         description='Demo application for vertebra classification.'
     )
-    parser.add_argument('--images', required=True,
+    parser.add_argument('--images', type=Path, required=True,
                         help='Path to dir with images for classification. '
                              'It also can be a markup file for evaluation.')
-    parser.add_argument('--model-path', required=True,
+    parser.add_argument('--model-path', type=Path, required=True,
                         help='Path to trained model or dir with models.')
-    parser.add_argument('--save-to',
+    parser.add_argument('--save-to', type=Path,
                         help='Path to dir for store classification results '
                              'instead of displaying them.')
     parser.add_argument('--iou-th', type=float, default=0.5,
                         help='Arg for remove intersecting boxes.')
 
-    args = parser.parse_args()
-    if args.save_to is not None:
-        os.makedirs(args.save_to, exist_ok=True)
+    return parser.parse_args()
 
-    return args
+
+def read_model(model_path, device):
+    """Read and prepare model and transforms for prediction."""
+    if model_path.suffix == '.pth':
+        ckpt = torch.load(model_path, map_location=device)
+        model = models.detection.fasterrcnn_resnet50_fpn(num_classes=3)
+        model.load_state_dict(ckpt['model'])
+
+        rss = ckpt['args'].resize_size
+        ccs = ckpt['args'].center_crop_size
+        mv = ckpt['args'].min_visibility
+        mean = ckpt['args'].mean
+        std = ckpt['args'].std
+
+    elif model_path.suffix == '.zip':
+        model = torch.jit.load(str(model_path))
+
+        rss = model.resize_size
+        ccs = model.center_crop_size
+        mv = model.min_visibility
+        mean = model.mean
+        std = model.std
+
+    else:
+        raise AttributeError('wrong model type (expected .pth or .zip ,'
+                             ' got {}.'.format(model_path.suffix))
+
+    model.to(device)
+    model.eval()
+
+    vis_transform, _ = get_test_transform(rss, ccs, mv)
+    to_tensor_transform = ToTensor(normalize={'mean': mean, 'std': std})
+
+    return model, vis_transform, to_tensor_transform
 
 
 def main():
     """Application entry point."""
     args = get_args()
+    if args.save_to is not None:
+        args.save_to.mkdir(exist_ok=True, parents=True)
 
-    markup = False
-    evaluator = None
-    if os.path.isfile(args.images):
+    if args.images.is_file():
         markup = True
         evaluator = Evaluator()
 
-        with open(args.images) as f:
+        with open(str(args.images)) as f:
             samples = json.load(f)
 
     else:
-        samples = [os.path.join(args.images, path)
-                   for path in os.listdir(args.images)]
+        markup = False
+        evaluator = None
+        samples = [str(x) for x in args.images.glob('*')]
 
-    device = torch.device('cpu')
-    if torch.cuda.is_available():
-        device = torch.device('cuda')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    if os.path.isfile(args.model_path):
+    # prepare models paths
+    if args.model_path.is_file():
         models_paths = [args.model_path]
     else:
-        models_paths = [os.path.join(args.model_path, name)
-                        for name in os.listdir(args.model_path)]
+        models_paths = list(args.model_path.glob('*'))
 
     for model_path in models_paths:
-        # model preparation
-        ckpt = torch.load(model_path, map_location=device)
-
-        model = models.detection.fasterrcnn_resnet50_fpn(pretrained=False,
-                                                         num_classes=3)
-        model.load_state_dict(ckpt['model'])
-        model.to(device)
-        model.eval()
-
-        # create transform for visualization only (without ToTensor)
-        vis_transform, _ = get_test_transform(
-            ckpt['args'].resize_size,
-            ckpt['args'].center_crop_size,
-            ckpt['args'].min_visibility
-        )
-        to_tensor_transform = ToTensor(
-            normalize={'mean': ckpt['args'].mean, 'std': ckpt['args'].std}
+        model, vis_transform, to_tensor_transform = read_model(
+            model_path, device
         )
 
         cpu_device = torch.device('cpu')
         with torch.no_grad():
             for sample in tqdm(samples, desc='Predicting'):
                 # get and process output
-                if markup:
-                    img_path = sample['img_path']
-                else:
-                    img_path = sample
-
+                img_path = sample['img_path'] if markup else sample
                 img = cv2.imread(img_path)
                 if markup:
                     sample = {
@@ -107,11 +118,7 @@ def main():
                                    for x in sample['annotation']]
                     }
                 else:
-                    sample = {
-                        'image': img,
-                        'bboxes': [],
-                        'labels': []
-                    }
+                    sample = {'image': img, 'bboxes': [], 'labels': []}
 
                 # prepare image for visualization
                 sample = vis_transform(**sample)
@@ -123,6 +130,11 @@ def main():
 
                 # applying model
                 output = model([sample['image']])
+
+                # for fasterrcnn from torchscript it returns [losses, outputs]
+                if len(output) == 2:
+                    output = output[1]
+
                 output = {k: v.to(cpu_device).numpy()
                           for k, v in output[0].items()}
                 output = postprocessing([output], iou_th=args.iou_th)[0]
@@ -152,21 +164,19 @@ def main():
                     cv2.waitKey()
 
                 else:
-                    model_save_folder = os.path.join(
-                        args.save_to,
-                        os.path.basename(model_path)[:-4] + '_preds'
-                    )
-                    os.makedirs(model_save_folder, exist_ok=True)
-                    save_path = os.path.join(model_save_folder,
-                                             os.path.basename(img_path))
-                    cv2.imwrite(save_path, drawn_img)
+                    model_save_folder = args.save_to.joinpath(
+                        model_path.stem + '_preds')
+                    model_save_folder.mkdir(exist_ok=True, parents=True)
+
+                    save_path = model_save_folder.joinpath(Path(img_path).name)
+                    cv2.imwrite(str(save_path), drawn_img)
 
         # calculate metrics if markup file was passed
         if markup:
-            print('\nMetrics for model:', os.path.basename(model_path))
+            print('\nMetrics for model:', model_path.name)
             metrics = evaluator.calculate_metrics()
             for name, value in metrics.items():
-                print(f'{name}: {value}')
+                print('{}: {}'.format(name, value))
 
 
 if __name__ == '__main__':
