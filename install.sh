b167c90eb1c34739c269046b66e9b4d923bbcd1e
Vladislav <v.dusiak@gmail.com>
2020-05-03 12:45:52 +0300

    Minor improvement for install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ REPODIR="$(cd "$(dirname "$0")" || exit; pwd -P)"
 cd "$REPODIR" || exit;
 
 sudo apt update
-sudo apt install git-lfs python3-pip libgtk2.0-dev pkg-config
+sudo apt install -y git-lfs python3-pip libgtk2.0-dev pkg-config
 git-lfs pull
 sudo -H pip3 install -r requirements.txt
